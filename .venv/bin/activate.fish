9a053c941e6aac3dbbd36a094d4d01b04b49d090
joshitachu <118347322+joshitachu@users.noreply.github.com>
2026-03-08 23:36:33 +0100

    Update notebook en voeg .gitignore toe

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /Users/joshuakabel/Documents/Source/Repos/machinelearning/.venv
+set -gx VIRTUAL_ENV /Users/joshuakabel/Desktop/machinelearningfix/Machiene-learning-sleep/.venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
